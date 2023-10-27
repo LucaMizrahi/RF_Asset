470db56b3259af91392e831dadba3522cffbf786
Luca Mizrahi <lucam1@al.insper.edu.br>
2023-10-27 18:38:15 -0300

    Código quase finalizado

--- BrazilYieldCurve/DI_pre.py
+++ BrazilYieldCurve/DI_pre.py
@@ -18,36 +18,52 @@ def yieldsbr(Initial_Date, Final_Date, Maturities, output_file):
             if table is not None:
                 rows = table.find_all('td')
                 td_text = [td.get_text() for td in rows]
-                #print(td_text)
+
                 # Cria a matriz data que será usada para criar a spline
-                data = pd.DataFrame({'Dias Corridos': td_text[::3],
+                data = pd.DataFrame({'Dias Corridos': [int(td_text[i]) for i in range(0, len(td_text), 3)],
                     'V2': [float(td_text[i].replace(',', '.')) for i in range(1, len(td_text), 3)],
                     'V3': [float(td_text[i].replace(',', '.')) for i in range(2, len(td_text), 3)]})
-                print(data)
-
-                
-                    # data.append([col.text.strip().replace(".", "").replace(",", ".") for col in cols])
-                # data = np.array(data, dtype=float)
-                # t = data[:, 0] / 21
-                # y = data[:, 1]
-                # spl = UnivariateSpline(t, y)
-                # t_new = np.array(Maturities)
-                # new = spl(t_new)
-                # mat[i, :] = new
+                #print(data)
+                '''	t = np.array(data[:, 1], dtype=int) / 21
+                    y = np.array(data[:, 2], dtype=float)
+                    spl = UnivariateSpline(t, y, s=0)
+                    t_new = Maturities
+                    new = spl(t_new)
+                    mat[i, :] = new'''
+                # Realiza a interpolação dos dados e cria a matriz de retorno
+                data_to_array = data.to_numpy()
+                t = np.array(data_to_array[:, 0], dtype=int) / 21
+                y = np.array(data_to_array[:, 1], dtype=float)
+                spl = UnivariateSpline(t, y, s=0)
+                t_new = np.array(Maturities)
+                new = spl(t_new)
+                for i in range(len(dates)):
+                    mat[i, :] = new
+                #print(mat)
+            
             else:
                 print(f"No tables found for {date}")
         else:
             print(f"Failed to retrieve data for {date}")
+    
+    '''colnames = ["M" + str(m) for m in Maturities]
+    mat = mat[np.apply_along_axis(np.isfinite, 1, mat).all(axis=1)]
+    return pd.DataFrame(mat, index=dates, columns=colnames)'''
 
     # Create a DataFrame with the results
-    df = pd.DataFrame(mat, columns=[f'M{m}' for m in Maturities], index=dates)
+    colnames = [f"M{m}" for m in Maturities]
+    mat = mat[np.apply_along_axis(np.isfinite, 1, mat).all(axis=1)]
+
+    # Constrói DataFrame final e salva em CSV
+    df = pd.DataFrame(mat, index=dates, columns=colnames)
+    print(df)
 
     # Save the DataFrame to a CSV file
     df.to_csv(output_file)
 
 # Example
-Initial_Date = '2023-10-25'  # Available from 2003-08-08. YYYY-MM-DD
-Final_Date = '2023-10-25'
+Initial_Date = '2019-01-01'  # Available from 2003-08-08. YYYY-MM-DD
+Final_Date = '2019-07-08'
 Maturities = [1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 48, 60, 72]
 output_file = 'output.csv'
 
